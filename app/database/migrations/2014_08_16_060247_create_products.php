e7d58b16d383d3fcdb471832dea223f7020fae47
chaos29092 <chaos29092@gmail.com>
2014-08-16 20:52:37 +0800

    数据库改为dto，重新调整了migrations和seeder，下面需要测试存储HTML到底需不需要e()函数来转义

--- app/database/migrations/2014_08_16_060247_create_products.php
+++ app/database/migrations/2014_08_16_060247_create_products.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddProducts extends Migration {
+class CreateProducts extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,11 @@ class AddProducts extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('items',function($table){
+		Schema::create('products',function($table){
             $table->increments('id');
             $table->string('name');
             $table->boolean('is_discount');
             $table->string('category');
-            $table->string('tag');
             $table->string('mainphoto');
             $table->text('introduction');
             $table->string('appphoto');
@@ -40,7 +39,7 @@ class AddProducts extends Migration {
 	 */
 	public function down()
 	{
-        Schema::drop('items');
+        Schema::drop('products');
 	}
 
 }
